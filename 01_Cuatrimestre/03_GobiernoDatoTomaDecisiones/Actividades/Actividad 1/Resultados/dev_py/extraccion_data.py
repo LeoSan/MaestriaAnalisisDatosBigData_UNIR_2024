b82f983e70fd1fdd3bb827840ad1ac1c7f86c868
LeoSan <cuenca623@gmail.com>
2025-01-05 16:40:24 -0600

    update python

--- 01_Cuatrimestre/03_GobiernoDatoTomaDecisiones/Actividades/Actividad 1/Resultados/dev_py/extraccion_data.py
+++ 01_Cuatrimestre/03_GobiernoDatoTomaDecisiones/Actividades/Actividad 1/Resultados/dev_py/extraccion_data.py
@@ -1,8 +1,9 @@
 import csv
+import pandas as pd
 
 note = ''
 
-with open('03032023.csv', newline='') as f:
+with open('/Users/leonard/Documents/MaestriaAnalisisDatosBigData_UNIR_2024/01_Cuatrimestre/03_GobiernoDatoTomaDecisiones/Actividades/Actividad 1/Resultados/dev_py/03032023.csv', newline='') as f:
     data  = csv.reader(f, delimiter=',')
     notes = list(data)
 
@@ -19,7 +20,8 @@ def getData(notes):
                 list_limpia.append(notes[i][j])
         #print(list_limpia)
         data.append(list_limpia)
-        print(data)
-        print('')
+    
+    print(data)
+    print('')
 
 getData(notes)
